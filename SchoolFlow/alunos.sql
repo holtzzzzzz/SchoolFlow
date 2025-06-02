d340555cd623985463f7643756beec8cecd3c3e0
Guilherme Holtz <guiguiholtz@gmail.com>
2025-06-02 11:28:08 -0300

    Update alunos.sql

--- SchoolFlow/alunos.sql
+++ SchoolFlow/alunos.sql
@@ -1,5 +1,8 @@
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
   username VARCHAR(50) UNIQUE NOT NULL,
-  password VARCHAR(255) NOT NULL
+  password VARCHAR(255) NOT NULL,
+  nome_completo VARCHAR(255),
+  data_nascimento DATE,
+  funcao VARCHAR(50)
 );
